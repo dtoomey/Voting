6d267cf8336d8cd2cc47ad0d4c75b8548eb9fb9e
Dan Toomey <dan@mexia.com.au>
2017-11-05 20:51:34 +1000

    Fixed up PowerShell deployment scripts

--- SF_DeployApplication.ps1
+++ SF_DeployApplication.ps1
@@ -2,24 +2,31 @@
 ##  REF: https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-automate-powershell
 ###################################################################################################
 
+Import-Module "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\Tools\PSModule\ServiceFabricSDK\ServiceFabricSDK.psm1"
+
+$path = 'C:\Users\Dan\Documents\Visual Studio 2015\Projects\Voting\Voting\pkg\Debug'
+$imageStorePath = 'Voting'
+$appTypeName = 'VotingType'
+$appVersion = '1.0.0'
+$clusterAddress = 'localhost:19000'
 
 # Connect to the Service Fabric cluster
-Connect-ServiceFabricCluster localhost:19000
+Connect-ServiceFabricCluster $clusterAddress
+
+# Upload package to package store
+Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $path -ApplicationPackagePathInImageStore $imageStorePath -ImageStoreConnectionString (Get-ImageStoreConnectionStringFromClusterManifest(Get-ServiceFabricClusterManifest)) -TimeoutSec 1800
+
+# Register the application package
+Register-ServiceFabricApplicationType $imageStorePath
 
 # Get all application types registered in the cluster
 Get-ServiceFabricApplicationType
+
+# Create the application 
+New-ServiceFabricApplication "fabric:/$imageStorePath" $appTypeName $appVersion
 
 # Get all application instances created in the cluster
 Get-ServiceFabricApplication
 
 # Get all service instances for each application
 Get-ServiceFabricApplication | Get-ServiceFabricService
-
-# Remove an application instance
-Remove-ServiceFabricApplication fabric:/Voting
-
-# Unregister the application type
-Unregister-ServiceFabricApplicationType VotingType 1.0.0
-
-# Remove the application package
-Remove-ServiceFabricApplicationPackage -ImageStoreConnectionString file:C:\SfDevCluster\Data\ImageStoreShare -ApplicationPackagePathInImageStore Voting
